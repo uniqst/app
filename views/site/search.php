0fcb2fe6b0ff9ffe569540809257d9294f16e411
Galifax <galifax94@gmail.com>
2017-04-27 13:26:33 +0300

    fix search

--- views/site/search.php
+++ views/site/search.php
@@ -13,11 +13,7 @@ $this->title = 'Поиск|'.$q;
         <h2>Поиск по запросу: <?=$q?>, найдено <?=count($product)?> товаров</h2>
         <div class="site-index">
             <div class="body-content">
-                <div class="row products">
-                    <?php foreach($product as $prod){
-                        include "_product.php";
-                    }?>
-              
+              <?=$this->render('_prod', compact('product'));?>
                 </div>
                 <div class="clearfix"></div>
                 <?=LinkPager::widget(['pagination' => $pagination])?>
